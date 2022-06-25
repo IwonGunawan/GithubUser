3ff14ed2591e07d32c89e4d82ab22f18aa6935e5
Iwon <iwongunawan@gmail.com>
2022-06-25 14:23:59 +0700

    Change class to object for ApiConfig

--- app/src/main/java/com/iwon/githubuser/api/ApiConfig.kt
+++ app/src/main/java/com/iwon/githubuser/api/ApiConfig.kt
@@ -7,28 +7,25 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ApiConfig {
-    /*
-    this class for handle networking
-     */
-    companion object{
-        fun getApiService() : ApiService{
-            val loggingInterceptor = if (BuildConfig.DEBUG){
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-            }else{
-                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-            }
-            val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .build()
-            val retrofit = Retrofit.Builder()
-                .baseUrl(GlobalVariable.endPoint)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
+object ApiConfig {
 
-            return retrofit.create(ApiService::class.java)
+    // this class for handle networking
+    fun getApiService() : ApiService{
+        val loggingInterceptor = if (BuildConfig.DEBUG){
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        }else{
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
         }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(GlobalVariable.endPoint)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
     }
 
 
